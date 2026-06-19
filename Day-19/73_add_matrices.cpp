656b0450bb9595357a195700b0f6f9b62052e626
Shubham Yadav <contact.shubham1124@gmail.com>
2026-06-19 10:06:55 +0530

    Solved problems for Day 19

---
+++ Day-19/73_add_matrices.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int nums1[3][3], nums2[3][3];
+    cout << "enter the elements of the first (3x3) matrix: ";
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            cin >> nums1[i][j];
+        }
+    }
+    cout << "enter the elements of the second (3x3) matrix: ";
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            cin >> nums2[i][j];
+        }
+    }
+    int sum_matrix[3][3];
+
+    for(int i = 0 ; i < 3 ; i++){
+        for(int j = 0 ; j < 3; j++){
+            sum_matrix[i][j]=nums1[i][j]+nums2[i][j];
+            cout << sum_matrix[i][j] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
